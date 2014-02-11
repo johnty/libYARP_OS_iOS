9e92633774bea39611c3ac12d7cbf22ae23d240b
johnty <johntywang@gmail.com>
2014-02-11 13:30:46 -0800

    adding to repo

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/johnty/Documents/yarp-2.3.22_iOS/src/libYARP_OS
+# Build directory: /Users/johnty/Documents/yarp-2.3.22_iOS/src/libYARP_OS
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
